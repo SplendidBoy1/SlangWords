3bef055053c9864688bb9294798b19f513c1cb6f
lxdat21@clc.fitus.edu.vn <lexuandat6062@gmail.com>
2024-11-19 19:40:19 +0700

    read file

--- slangword/src/main/java/main/App.java
+++ slangword/src/main/java/main/App.java
@@ -6,10 +6,16 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderStrokeStyle;
+import javafx.scene.layout.BorderWidths;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 /**
@@ -21,7 +27,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        Dictionary_Slang dict = new Dictionary_Slang();
+        Dictionary_Slang dict = new Dictionary_Slang<String, String>();
         dict.LoadData();
         GridPane root = new GridPane();
         Button btn_search = new Button("Search Dictionary");
@@ -29,15 +35,24 @@ public class App extends Application {
         Button btn_learn = new Button("Learn Slang Word");
         Search_feature Search = new Search_feature();
         Edit_feature Edit = new Edit_feature();
-        root.setPadding(new Insets(30, 30, 30, 30)); 
+        root.setPadding(new Insets(30, 10, 30, 10)); 
         root.setVgap(20); 
-
-        root.setAlignment(Pos.TOP_CENTER);
-        root.add(btn_search, 1, 0);
-        root.add(btn_edit, 1, 1);
-        root.add(btn_learn, 1, 2);
+        root.setHgap(20);
+        Pane pane_1 = new Pane();
+        pane_1.getChildren().add(btn_search);
+        root.setGridLinesVisible( true );
+        // pane_1.setBorder(new Border(new BorderStroke(Color.BLACK, 
+        //     BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
+        Pane pane_2 = new Pane();
+        pane_2.getChildren().add(btn_edit);
+        // root.setAlignment(Pos.TOP_CENTER);
         
-        scene = new Scene(root, 400, 300);
+        root.add(pane_1, 0, 0, 2, 1);
+        root.add(pane_2, 2, 0);
+        root.add(btn_learn, 1, 1);
+        // root.setColumnSpan(pane_1, 1);
+        pane_1.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+        scene = new Scene(root, 700, 600);
         btn_search.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event){
--- slangword/src/main/java/main/Dictionary_Slang.java
+++ slangword/src/main/java/main/Dictionary_Slang.java
@@ -6,8 +6,9 @@ import java.io.IOException;
 import java.util.Dictionary;
 import java.util.Hashtable;
 
-class  Dictionary_Slang {
-    private Dictionary<String, Integer> _slang_dict= new Hashtable<>();
+
+class  Dictionary_Slang<K, V> {
+    private Dictionary<K, V> _slang_dict= new Hashtable<>();
 
 
     public void LoadData() throws IOException{
@@ -21,8 +22,30 @@ class  Dictionary_Slang {
         BufferedReader br = new BufferedReader(new FileReader("slangword/data_input/slang.txt"));
         String line = br.readLine();
         while(line != null){
-            System.out.println(line);
+            String each_part[] = line.split("`");
+            // System.out.println(each_part[0]);
+            // System.out.println(each_part[1]);
+            // System.out.println(line);
+            _slang_dict.put((K)each_part[0].toLowerCase(), (V)each_part[1]);
             line = br.readLine();
         }
+
+        long startTime = System.nanoTime();
+        
+
+        _slang_dict.keys().asIterator().forEachRemaining((key -> {
+
+            String temp = (String)_slang_dict.get(key);
+            if (temp.toLowerCase().contains("today")) {
+                // System.out.println(temp);
+            }
+        }));
+
+        long endTime = System.nanoTime();
+
+        long duration = (endTime - startTime);
+        System.out.println(duration);
+        // System.out.println(_slang_dict.keys());
+        // System.out.println(_slang_dict.get("wtft"));
     }
 }
